4e753f6c19a8aba5e15143b4b301e54fea8f5dd8
Carson McDonald <carson@ioncannon.net>
2017-02-18 14:14:40 -0500

    Fix compile error

--- components/mruby_component/component.mk
+++ components/mruby_component/component.mk
@@ -4,6 +4,8 @@ COMPONENT_OWNCLEANTARGET := true
 
 COMPONENT_ADD_INCLUDEDIRS := mruby/include/
 
+CFLAGS += -Wno-char-subscripts
+
 build: 
 	cd $(COMPONENT_PATH)/mruby && MRUBY_CONFIG=../esp32_build_config.rb $(MAKE)
 	cp $(COMPONENT_PATH)/mruby/build/esp32/lib/libmruby.a $(COMPONENT_LIBRARY)
