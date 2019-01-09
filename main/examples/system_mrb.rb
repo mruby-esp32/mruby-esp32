b949ba32752a231c661b60398003d0ac70f3415e
YAMAMOTO Masaya <pandax381@gmail.com>
2019-01-09 14:44:45 +0900

    Update example script

--- main/examples/system_mrb.rb
+++ main/examples/system_mrb.rb
@@ -1,13 +1,18 @@
+puts "RUBY_VERSION: #{RUBY_VERSION}"
+puts "RUBY_ENGINE: #{RUBY_ENGINE}"
+puts "RUBY_ENGINE_VERSION: #{RUBY_ENGINE_VERSION}"
+puts "MRUBY_VERSION: #{MRUBY_VERSION}"
+puts "MRUBY_RELEASE_NO: #{MRUBY_RELEASE_NO}"
+puts "MRUBY_RELEASE_DATE: #{MRUBY_RELEASE_DATE}"
+puts "MRUBY_DESCRIPTION: #{MRUBY_DESCRIPTION}"
+puts "MRUBY_COPYRIGHT: #{MRUBY_COPYRIGHT}"
+puts
 ver = ESP32::System.sdk_version
-puts "SDK Version: #{ver}"
-
+puts "ESP32 SDK Version: #{ver}"
 mem = ESP32::System.available_memory / 1000
-puts "Memory free: #{mem}K"
-
-puts "Delaying 10 seconds"
+puts "Available Memory: #{mem}K"
+puts
+puts "Delaying 10 seconds..."
 ESP32::System.delay(10 * 1000)
-
-#ESP32::System.restart()
-
-puts "Deep sleeping for 10 seconds"
+puts "Deep sleeping for 10 seconds..."
 ESP32::System.deep_sleep_for(10 * 1000000)
