bb234ea379215293a3bbf3717d588128052d1b5d
kishima <kishima7@gmail.com>
2019-07-21 01:18:21 +0900

    fix compile error

--- main/component.mk
+++ main/component.mk
@@ -2,11 +2,11 @@ COMPONENT_DEPENDS := mruby_component FabGL_component arduino
 
 COMPONENT_EXTRA_INCLUDES := $(PROJECT_PATH)/components/FabGL_component/src
 
-COMPONENT_EXTRA_CLEAN := example_mrb.h
+#COMPONENT_EXTRA_CLEAN := example_mrb.h
 
-mruby_main.o: example_mrb.h
+#mruby_main.o: example_mrb.h
 
-example_mrb.h: $(COMPONENT_PATH)/examples/$(MRUBY_EXAMPLE)
-	$(COMPONENT_PATH)/../components/mruby_component/mruby/bin/mrbc -B example_mrb -o $@ $^
+#example_mrb.h: $(COMPONENT_PATH)/examples/$(MRUBY_EXAMPLE)
+#	$(COMPONENT_PATH)/../components/mruby_component/mruby/bin/mrbc -B example_mrb -o $@ $^
 
-.PHONY: example_mrb.h
+#.PHONY: example_mrb.h
