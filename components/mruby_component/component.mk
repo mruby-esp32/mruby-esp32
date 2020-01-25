d0ddc50f5c326b6cbde7acce06f215f63ad668ee
kishima <kishima7@gmail.com>
2020-01-25 16:20:35 +0900

    update mruby build config and tools

--- components/mruby_component/component.mk
+++ components/mruby_component/component.mk
@@ -2,14 +2,14 @@
 COMPONENT_OWNBUILDTARGET := true
 COMPONENT_OWNCLEANTARGET := true
 
-COMPONENT_EXTRA_INCLUDES := $(PROJECT_PATH)/components/FabGL_component/src
+COMPONENT_EXTRA_INCLUDES := $(PROJECT_PATH)/components/FabGL_component/src $(PROJECT_PATH)/main
 
 COMPONENT_ADD_INCLUDEDIRS := mruby/include/
 
 CFLAGS += -Wno-char-subscripts -Wno-pointer-sign
 
 build: 
-	cd $(COMPONENT_PATH)/mruby && COMPONENT_EXTRA_INCLUDES=$(COMPONENT_EXTRA_INCLUDES) MRUBY_CONFIG=../esp32_build_config.rb $(MAKE)
+	cd $(COMPONENT_PATH)/mruby && COMPONENT_EXTRA_INCLUDES="$(COMPONENT_EXTRA_INCLUDES)" MRUBY_CONFIG=../esp32_build_config.rb $(MAKE)
 	cp $(COMPONENT_PATH)/mruby/build/esp32/lib/libmruby.a $(COMPONENT_LIBRARY)
 
 clean: 
--- tool/img_to_rgb222.rb
+++ tool/img_to_rgb222.rb
@@ -2,17 +2,25 @@ require 'rubygems'
 require 'RMagick'
 
 #convert image.png -colors 64 -depth 2 +dither image3.png
-image = Magick::Image.read('image3.jpg').first
+#convert -resize 640x350! test2a.jpg test2c.jpg
+
+if ARGV.size != 2
+  puts "please set an input file path and an output file path."
+  exit
+end
+input_fname = ARGV[0]
+output_fname = ARGV[1]
+image = Magick::Image.read(input_fname).first
 
 height = image.rows
 width  = image.columns
-puts "w:#{width} h:#{height}"
+puts "#{input_fname} > w:#{width} h:#{height}"
 exit if height > 256*256
 exit if width > 256*256
 
 header = 0xFFFFFFFF
 
-File.open("./test.img","wb") do |fp|
+File.open(output_fname,"wb") do |fp|
   fp.write([ header ].pack("V")) #pack uint32_t
   fp.write([ width  ].pack("v")) #pack uint16_t little:v  big:n
   fp.write([ height ].pack("v"))
@@ -25,11 +33,10 @@ File.open("./test.img","wb") do |fp|
       r = (pixel.red*3 / 65535.0).round
       g = (pixel.green*3 / 65535.0).round
       b = (pixel.blue*3 / 65535.0).round
-      #val = (b << 4) | (g << 2) | r
 
       val = (a << 6) | (b << 4) | (g << 2) | r
       #puts "#{pixel.red},#{pixel.green},#{pixel.blue}"
-      puts "#{r},#{g},#{b}"
+      #puts "#{r},#{g},#{b}"
       fp.write([val].pack("C"))
     end 
   end
