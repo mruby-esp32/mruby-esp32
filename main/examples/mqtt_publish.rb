03a1eb3dcb6349d3feb9d848cab703379379c39b
yuuu <okazaki@fusic.co.jp>
2023-02-12 10:23:47 +0900

    Removed block for callback on connection from MQTT publish example.

--- main/examples/mqtt_publish.rb
+++ main/examples/mqtt_publish.rb
@@ -1,20 +1,14 @@
 wifi = ESP32::WiFi.new
 
-wifi.on_connected do |ip|
-  puts "Connected: #{ip}"
-
-  mqtt = ESP32::MQTT::Client.new('test.mosquitto.org', 1883)
-  mqtt.connect
-  mqtt.publish("mruby-esp32-mqtt", '{ "hello": "world." }')
-  mqtt.disconnect
-end
-
-wifi.on_disconnected do
-  puts 'Disconnected'
-end
-
 puts 'Connecting to wifi'
 wifi.connect('SSID', 'password')
+
+puts "Connected"
+
+mqtt = ESP32::MQTT::Client.new('test.mosquitto.org', 1883)
+mqtt.connect
+mqtt.publish("mruby-esp32-mqtt", '{ "hello": "world." }')
+mqtt.disconnect
 
 #
 # Loop forever otherwise the script ends
