bdabb5e1e2167b259c9e220f60266541ebdf8329
yuuu <okazaki@fusic.co.jp>
2023-07-30 06:24:06 +0900

    Add mruby-esp32-pwm.

---
+++ main/examples/pwm_servo.rb
@@ -0,0 +1,14 @@
+WAIT_MS = 1000
+pwm = PWM.new(0, freq: 50)
+
+def pulse_width_us(angle)
+  Rational((angle - (-90)) * (2500 - 500), (90 - (-90))).to_i + 500;
+end
+
+loop do
+  [0, 30, 60, 90, 60, 30, 0, -30, -60, -90, -60, -30].each do |angle|
+    puts "Angle=#{angle}"
+    pwm.pulse_width_us(pulse_width_us(angle))
+    ESP32::System.delay(WAIT_MS)
+  end
+end
